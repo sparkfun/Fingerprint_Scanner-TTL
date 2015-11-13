cfa50b4b6d999bc66386760118d73c5910fd3b07
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2015-11-13 12:46:38 +0530

    Minor Correction
    Changed Tx to Rx in comment.

--- FPS_GT511C3/Examples/FPS_Enroll/FPS_Enroll.ino
+++ FPS_GT511C3/Examples/FPS_Enroll/FPS_Enroll.ino
@@ -12,7 +12,7 @@
 
 // Hardware setup - FPS connected to:
 //	  digital pin 4(arduino rx, fps tx)
-//	  digital pin 5(arduino tx - 560ohm resistor fps tx - 1000ohm resistor - ground)
+//	  digital pin 5(arduino tx - 560ohm resistor fps rx - 1000ohm resistor - ground)
 //		this brings the 5v tx line down to about 3.2v so we dont fry our fps
 
 FPS_GT511C3 fps(4, 5);
