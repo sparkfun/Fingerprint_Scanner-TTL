b9944f9eef2bbbf82dd1d6a8a0c952a84621bbd4
Bobby <ho.chan@colorado.edu>
2017-09-25 02:22:26 -0600

    Update FPS_Blink.ino
    - added a description for this example
    - updated the hardware hookup
    - added option to adjust software serial for other Arduino microcontrollers (i.e. ATmega32U4, ATmega2560, etc.) due to limitations with the library.
    - added some a little bit more comments. =)
    - changed resistors used in voltage division:
    
    __Logic Level Conversion__
    
    I tested the setup with the previous resistors. I was not able to get it working with the GT511C1R model. However, I was able to get it working when using a voltage divider (with 10kΩ and 20kΩ) similar to the setup used in our previous logic level converter’s application circuit [ [https://cdn.sparkfun.com/assets/b/0/e/1/0/522637c6757b7f2b228b4568.png](https://cdn.sparkfun.com/assets/b/0/e/1/0/522637c6757b7f2b228b4568.png) ]. I believe the IC used in the GT511C1R requires a slightly higher logic level for the input voltage in order for the signal to be considered a logic HIGH by the fingerprint scanner [similar to this general explanation of 3.3V CMOS Logic Levels – [https://learn.sparkfun.com/tutorials/logic-levels#33-v-cmos-logic-levels(https://learn.sparkfun.com/tutorials/logic-levels#33-v-cmos-logic-levels) ].
    
    Doing a stress test (with the original 560Ohm and 1000Ohm resistor) and measuring the voltage with a multimeter, I noticed that the divided voltage was about 3.14V. When the GT511C1R was connected, the voltage dropped to about 2.97V. This is probably not high enough for the fingerprint scanner to recognize a logic HIGH.
    
    _Replace Resistors for Higher Logic Level Threshold_
    
    I was able to get the Arduino example code working by replacing the 560Ohm resistor with the 10kΩ resistor and replacing the 1000Ohm resistor with two 10kΩ resistors in series for 20kΩ. Testing the circuit with the resistors replaced, my multimeter read a divided voltage of 3.3V. When the GT511C1R was connected, the voltage dropped to about ~3.18V-3.04V. This was sufficient enough for the fingerprint scanner to receive the commands from the Arduino. I tested this with the blink examples and there were no problems.

--- FPS_GT511C3/Examples/FPS_Blink/FPS_Blink.ino
+++ FPS_GT511C3/Examples/FPS_Blink/FPS_Blink.ino
@@ -5,27 +5,72 @@
 	basically, Feel free to hack away at it, but just give me credit for my work =)
 	TLDR; Wil Wheaton's Law
 
-	This simple sketch turns the LED on and off similar to the Arduino blink sketch.
+        Description: This simple sketch turns the LED on and off similar to the Arduino blink sketch.
 	It is used to show that communications are working.
+	
+	This code should work with the any model of ADH-Tech's FPS as long as
+	you are within the minimum logic level threshold for the FPS serial UART.
+	This code has been tested with these models:
+ 
+              GT-511C3  [ https://www.sparkfun.com/products/11792 ]
+              GT-511C1R [ https://www.sparkfun.com/products/13007 ]
+	
 */
 
 #include "FPS_GT511C3.h"
 #include "SoftwareSerial.h"
 
-// Hardware setup - FPS connected to:
-//	  digital pin 4(arduino rx, fps tx)
-//	  digital pin 5(arduino tx - 560ohm resistor fps tx - 1000ohm resistor - ground)
-//		this brings the 5v tx line down to about 3.2v so we dont fry our fps
+/*-------------------- HARDWARE HOOKUP with 5V Arduino --------------------
+1.) Dedicated Bi-Directional Logic Level Converter (LLC)
 
-FPS_GT511C3 fps(4, 5);
+It is recommended to use a dedicated bi-direcitonal LLC
+[ https://www.sparkfun.com/products/12009 ] for a reliable connection if you
+are using a 5V Arduino microcontroller:
+
+   Fingerprint Scanner (Pin #) <-> Logic Level Converter <-> 5V Arduino w/ Atmega328P
+     UART_TX (3.3V TTL)(Pin 1) <->     LV1 <-> HV1       <->  RX (pin 4)
+     UART_RX (3.3V TTL)(Pin 2) <->     LV4 <-> HV4       <->  TX (pin 5)
+           GND         (Pin 3) <->     GND <-> GND       <->     GND
+      Vin (3.3V~6V)    (Pin 4) <->        HV             <->      5V
+                                          LV             <->     3.3V
+					  
+2.) Voltage Division w/ 3x 10kOhm Resistors
+
+Otherwise, you could use 3x 10kOhm resistors [ https://www.sparkfun.com/products/11508 ]
+to divide the voltage from a 5V Arduino down to 3.3V FPS similar to the
+"Uni-Directional" application circuit on our old logic level converter
+[ https://cdn.sparkfun.com/assets/b/0/e/1/0/522637c6757b7f2b228b4568.png ]:
+
+    Voltage Divider         <-> Fingerprint Scanner(Pin #) <-> Voltage Divider <-> 5V Arduino w/ Atmega328P
+                            <-> UART_TX (3.3V TTL) (Pin 1) <->                 <->       RX (pin 4)
+  GND <-> 10kOhm <-> 10kOhm <-> UART_RX (3.3V TTL) (Pin 2) <->      10kOhm     <->       TX (pin 5)
+          GND               <->        GND         (Pin 3) <->       GND       <->        GND
+                            <->    Vin (3.3V~6V)   (Pin 4) <->                 <->        5V
+			    
+Note: You can add the two 10kOhm resistors in series for 20kOhms. =)
+
+--------------------------------------------------------------------------------*/
+
+// set up software serial pins for Arduino's w/ Atmega328P's
+// FPS (TX) is connected to pin 4 (Arduino's Software RX)
+// FPS (RX) is connected through a converter to pin 5 (Arduino's Software TX)
+FPS_GT511C3 fps(4, 5); // (Arduino SS_RX = pin 4, Arduino SS_TX = pin 5)
+
+/*If using another Arduino microcontroller, try commenting out line 57 and
+uncommenting line 66 due to the limitations listed in the
+library's note => https://www.arduino.cc/en/Reference/softwareSerial . Do
+not forget to rewire the connection to the Arduino*/
+
+// FPS (TX) is connected to pin 10 (Arduino's Software RX)
+// FPS (RX) is connected through a converter to pin 11 (Arduino's Software TX)
+//FPS_GT511C3 fps(10, 11); // (Arduino SS_RX = pin 10, Arduino SS_TX = pin 11)
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(9600); //set up Arduino's hardware serial UART
 	fps.UseSerialDebug = true; // so you can see the messages in the serial debug screen
-	fps.Open();
+	fps.Open(); //send serial command to initialize fps
 }
-
 
 void loop()
 {
